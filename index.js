59dab84db1b3b998d762a51ea2359ab5435b89f0
wilmer rubio <rubiorojasw@gmail.com>
2020-02-24 14:41:55 -0500

    Create docker file

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 require('./app/worker/worker');
 
 // Listen requires
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3010;
 
 app.listen(port, () => {
   console.log(`Server started at port: ${port}`);
